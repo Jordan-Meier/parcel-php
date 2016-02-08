aaa2ab643cfaf0197e97044ae18643e0ff00adcb
Jordan Meier and Ryan Brown <jbirdmeier@gmail.com>
2016-02-08 13:18:56 -0800

    Add parcel class, constructor to parcel.php

---
+++ parcel.php
@@ -0,0 +1,34 @@
+<?php
+class Parcel
+{
+    private $weight;
+    private $height;
+    private $width;
+    private $length;
+
+    function __construct($parcel_weight, $parcel_height, $parcel_width, $parcel_length)
+    {
+        $this->weight = $parcel_weight;
+        $this->height = $parcel_height;
+        $this->width = $parcel_width;
+        $this->length = $parcel_length;
+    }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Send a parcel</title>
+</head>
+<body>
+    <h1>Parcel site!</h1>
+    <ul>
+        <?php
+
+        ?>
+    </ul>
+</body>
+</html>
